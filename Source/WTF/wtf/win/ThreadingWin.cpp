9f081042b3cc058d081d1d9209b0821c1a7f2236
Dylan Conway <dylan.conway567@gmail.com>
2026-03-05 23:17:32 -0800

    Revert "Fix thread_local destructors not running under -fno-c++-static-destructors (#172)"
    This reverts commit 625984a513690ce840cfd469bc13df423575f026.

--- Source/WTF/wtf/win/ThreadingWin.cpp
+++ Source/WTF/wtf/win/ThreadingWin.cpp
@@ -281,7 +281,7 @@ struct Thread::ThreadHolder {
     RefPtr<Thread> thread;
 };
 
-WTF_THREAD_LOCAL_ALWAYS_DESTROY thread_local static Thread::ThreadHolder s_threadHolder;
+thread_local static Thread::ThreadHolder s_threadHolder;
 
 Thread* Thread::currentMayBeNull()
 {
